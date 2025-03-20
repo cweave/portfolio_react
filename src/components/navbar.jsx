9636cb7c1f6342885e5b4867bac566042a7baf58
Christa Weaver <cweav3r@gmail.com>
2025-03-20 15:47:29 -0700

    responsive styles

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Hamburger from './hamburger';
 
 const NavBar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
   return (
-    <nav>
-      <div className='nav-bar'>
-        <div className='nav-bar__logo' role='img' aria-label='CW in a circle' title='Christa Weaver'></div>
-        <ul className='nav-bar__navigation'>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#about-christa'>About</a></li>
-          <li><a href='#projects'>Projects</a></li>
-          <li><a href='#contact'>Contact</a></li>
-        </ul>
+    <nav className='navbar'>
+      <div className='navbar__container'>
+        <div className='navbar__logo' role='img' aria-label='CW in a circle' title='Christa Weaver'></div>
+        <div className='menu-icon' onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`navbar__navigation  ${showNavbar && "active"}`}>
+          <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#about-christa'>About</a></li>
+            <li><a href='#projects'>Projects</a></li>
+            <li><a href='#contact'>Contact</a></li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
